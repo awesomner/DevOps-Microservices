baafd3cebdf652c654f77f94a223ec99aef50813
TomasS <tomas.sjekel@26house.com>
2022-04-23 19:12:44 +0200

    [UPD] run docker script

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag awesomner/udacity_ml_project .
 
-# Step 2: 
+# Step 2:
 # List docker images
+docker image ls
 
-# Step 3: 
+# Step 3:
 # Run flask app
+docker run -it --rm -p 8000:80 --name udacity_ml_project awesomner/udacity_ml_project
