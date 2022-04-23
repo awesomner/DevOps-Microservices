3cc06f1b7d69cc0b8084c8f194f0d31f40619418
TomasS <tomas.sjekel@26house.com>
2022-04-23 19:14:38 +0200

    [UPD] run kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="awesomner/udacity_ml_project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment prediction --image=awesomner/udacity_ml_project
+sleep 20
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/prediction 8000:80
