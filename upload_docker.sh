45f9a66f3efd8997eb0fb18cbbb6cce69ea82543
TomasS <tomas.sjekel@26house.com>
2022-04-23 19:12:56 +0200

    [UPD] upload docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="awesomner/udacity_ml_project"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image tag udacity_ml_project:latest awesomner/udacity_ml_project:latest
+docker push $dockerpath
